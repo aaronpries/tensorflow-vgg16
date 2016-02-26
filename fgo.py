34f2d7972a834f229a3bd6af5e448e7ad15affc7
Damien Firmenich <fir.damien@gmail.com>
2016-02-26 12:42:36 +0100

    ok

--- fgo.py
+++ fgo.py
@@ -20,18 +20,20 @@ class Model():
     return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
       padding='SAME', name=name)
 
-  def _conv_layer(self, bottom, name, shape_w, shape_b):
+  def _conv_layer(self, bottom, name, shape_f, shape_out):
+    shape_w = (shape_f[0], shape_f[1], bottom.get_shape()[-1].value, shape_out)
+
     with tf.variable_scope("conv") as scope:
       filt = self.get_conv_filter(name, shape_w)
       conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
 
-      conv_biases = self.get_bias_conv(name, shape_b)
+      conv_biases = self.get_bias_conv(name, (shape_out,))
       bias = tf.nn.bias_add(conv, conv_biases)
 
       relu = tf.nn.relu(bias)
       return relu
 
-  def _fc_layer(self, bottom, name, shape_w, shape_b):
+  def _fc_layer(self, bottom, name, shape_out):
     # with tf.variable_scope(name) as scope:
     shape = bottom.get_shape().as_list()
     dim = 1
@@ -39,8 +41,9 @@ class Model():
          dim *= d
     x = tf.reshape(bottom, [-1, dim])
 
+    shape_w = (x.get_shape()[-1].value, shape_out)
     weights = self.get_fc_weight(name, shape_w)
-    biases = self.get_bias_fc(name, shape_b)
+    biases = self.get_bias_fc(name, (shape_out,))
 
     # Fully connected layer. Note that the '+' operation automatically
     # broadcasts the biases.
@@ -48,10 +51,11 @@ class Model():
 
     return fc
 
-  def _fc_layer_mod(self, bottom, name, shape_w, shape_b):
+  def _fc_layer_mod(self, bottom, name, shape_out):
     # with tf.variable_scope(name) as scope:
+    shape_w = (bottom.get_shape()[-1].value, shape_out)
     weights = self.get_fc_weight_mod(name, shape_w)
-    biases = self.get_bias_mod(name, shape_b)
+    biases = self.get_bias_mod(name, (shape_out,))
 
     fc = tf.nn.bias_add(tf.matmul(bottom, weights), biases)
     return fc
@@ -59,7 +63,7 @@ class Model():
 
   # Input should be an rgb image [batch, height, width, 3]
   # values scaled [0, 1]
-  def graph(self, X):
+  def graph(self, X, n_classes, mod=False):
     # self.images = tf.placeholder("float", [None, 224, 224, 3], name="images")
     # rgb = self.images
     rgb_scaled = X * 255.0
@@ -77,45 +81,48 @@ class Model():
     assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
 
     with tf.variable_scope("conv1"):
-      self.relu1_1 = self._conv_layer(bgr, "conv1_1", (3,3,3,64), (64,))
-      self.relu1_2 = self._conv_layer(self.relu1_1, "conv1_2", (3,3,64,64), (64,))
-      self.pool1 = self._max_pool(self.relu1_2, 'pool')
+      relu1_1 = self._conv_layer(bgr, "conv1_1", (3,3), 64)
+      relu1_2 = self._conv_layer(relu1_1, "conv1_2", (3,3), 64)
+      pool1 = self._max_pool(relu1_2, 'pool')
 
     with tf.variable_scope("conv2"):
-      self.relu2_1 = self._conv_layer(self.pool1, "conv2_1", (3,3,64,128), (128,))
-      self.relu2_2 = self._conv_layer(self.relu2_1, "conv2_2", (3,3,128,128), (128,))
-      self.pool2 = self._max_pool(self.relu2_2, 'pool')
+      relu2_1 = self._conv_layer(pool1, "conv2_1", (3,3), 128)
+      relu2_2 = self._conv_layer(relu2_1, "conv2_2", (3,3), 128)
+      pool2 = self._max_pool(relu2_2, 'pool')
 
     with tf.variable_scope("conv3"):
-      self.relu3_1 = self._conv_layer(self.pool2, "conv3_1", (3,3,128,256), (256,))
-      self.relu3_2 = self._conv_layer(self.relu3_1, "conv3_2", (3,3,256,256), (256,))
-      self.relu3_3 = self._conv_layer(self.relu3_2, "conv3_3", (3,3,256,256), (256,))
-      self.pool3 = self._max_pool(self.relu3_3, 'pool')
+      relu3_1 = self._conv_layer(pool2, "conv3_1", (3,3), 256)
+      relu3_2 = self._conv_layer(relu3_1, "conv3_2", (3,3), 256)
+      relu3_3 = self._conv_layer(relu3_2, "conv3_3", (3,3), 256)
+      pool3 = self._max_pool(relu3_3, 'pool')
 
     with tf.variable_scope("conv4"):
-      self.relu4_1 = self._conv_layer(self.pool3, "conv4_1", (3,3,256,512), (512,))
-      self.relu4_2 = self._conv_layer(self.relu4_1, "conv4_2", (3,3,512,512), (512,))
-      self.relu4_3 = self._conv_layer(self.relu4_2, "conv4_3", (3,3,512,512), (512,))
-      self.pool4 = self._max_pool(self.relu4_3, 'pool')
+      relu4_1 = self._conv_layer(pool3, "conv4_1", (3,3), 512)
+      relu4_2 = self._conv_layer(relu4_1, "conv4_2", (3,3), 512)
+      relu4_3 = self._conv_layer(relu4_2, "conv4_3", (3,3), 512)
+      pool4 = self._max_pool(relu4_3, 'pool')
 
     with tf.variable_scope("conv5"):
-      self.relu5_1 = self._conv_layer(self.pool4, "conv5_1", (3,3,512,512), (512,))
-      self.relu5_2 = self._conv_layer(self.relu5_1, "conv5_2", (3,3,512,512), (512,))
-      self.relu5_3 = self._conv_layer(self.relu5_2, "conv5_3", (3,3,512,512), (512,))
-      self.pool5 = self._max_pool(self.relu5_3, 'pool')
+      relu5_1 = self._conv_layer(pool4, "conv5_1", (3,3), 512)
+      relu5_2 = self._conv_layer(relu5_1, "conv5_2", (3,3), 512)
+      relu5_3 = self._conv_layer(relu5_2, "conv5_3", (3,3), 512)
+      pool5 = self._max_pool(relu5_3, 'pool')
 
     with tf.variable_scope("fc6"):
       drop = dropout(0.5)
-      self.fc6 = self._fc_layer(self.pool5, "fc6", (25088, 4096), (4096,))
-      self.relu6 = tf.nn.dropout(tf.nn.relu(self.fc6), drop)
+      fc6 = self._fc_layer(pool5, "fc6", 4096)
+      relu6 = tf.nn.dropout(tf.nn.relu(fc6), drop)
 
     with tf.variable_scope("fc7"):
       drop = dropout(0.5)
-      self.fc7 = self._fc_layer(self.relu6, "fc7", (4096,4096), (4096,))
-      self.relu7 = tf.nn.dropout(tf.nn.relu(self.fc7), drop)
+      fc7 = self._fc_layer(relu6, "fc7", 4096)
+      relu7 = tf.nn.dropout(tf.nn.relu(fc7), drop)
 
     with tf.variable_scope("fc8"):
-      logits = self._fc_layer_mod(self.relu7, "fc8", (4096,61), (61,))
+      if mod:
+        logits = self._fc_layer_mod(relu7, "fc8", n_classes)
+      else:
+        logits = self._fc_layer(relu7, "fc8", n_classes)
 
     return logits
 
@@ -174,10 +181,10 @@ def accuracy(prob, labels):
     in_top_k = tf.nn.in_top_k(prob, labels, 1)
     return tf.reduce_sum(tf.cast(in_top_k, tf.float32))
 
-def init(model, batch_size):
+def init(model, batch_size, n_classes):
   images = tf.placeholder("float", [None, 224, 224, 3], name="images")
   labels = tf.placeholder(tf.int32, [None], name="labels")
-  logits = model.graph(images)
+  logits = model.graph(images, n_classes)
   prob = inference(logits)
   loss = cost(logits, labels, batch_size)
   return images, labels, prob, loss
--- fgo_train.py
+++ fgo_train.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 import utils
 import sys
 import random
+import math
 import numpy as np
 
 import fgo
@@ -29,11 +30,14 @@ def evaluate(sess, images, labels, accuracy_op, validation_set, batch_size):
   return mean_correct, correct, total
 
 
-def main(saved, save_to, logdir, batch_size, steps):
+def main(saved, save_to, logdir, batch_size, steps, eval_size):
   train_set, validation_set, test_set = input_data.make_split(DATA_FOLDER)
   train_batches = input_data.load_batches(train_set, batch_size)
 
-  images, labels, pred_op, loss_op = fgo.init(fgo.FGO16(), batch_size)
+  if eval_size > 0:
+    validation_set = validation_set[:eval_size]
+
+  images, labels, pred_op, loss_op = fgo.init(fgo.FGO16(), batch_size, n_classes=61)
   saver = tf.train.Saver()
   train_op, global_step = fgo.training(loss_op)
   summary_op = fgo.summaries(loss_op)
@@ -58,11 +62,14 @@ def main(saved, save_to, logdir, batch_size, steps):
       summary_writer.add_summary(summary, step)
       print("Step %d, loss: %f" % (step, loss))
 
-      if step % 100 == 0:
+      if step % math.ceil(steps/100) == 0:
         path =saver.save(sess, save_to, global_step=step)
         print("Saved model to %s" % path)
         accuracy, correct, total = evaluate(sess, images, labels, accuracy_op, validation_set, batch_size)
         print("Accuracy on validation set: %.3f%% (%d/%d)" % (accuracy*100, correct, total))
+
+      if step >= steps:
+        break
 
 
 if __name__ == '__main__':
@@ -71,6 +78,7 @@ if __name__ == '__main__':
   parser.add_argument('export')
   parser.add_argument('--logdir', default="/tmp/fgo16")
   parser.add_argument('--batch', default=64, type=int)
-  parser.add_argument('--steps', default=100000, type=int)
+  parser.add_argument('--steps', default=1000, type=int)
+  parser.add_argument('--eval_size', default=0, type=int)
   args = parser.parse_args()
-  main(args.saved, args.export, args.logdir, args.batch, args.steps)
+  main(args.saved, args.export, args.logdir, args.batch, args.steps, args.eval_size)
